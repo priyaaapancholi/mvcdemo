f05da16ff23eba64d5c22b480ecc623b6774635d
Priya <priya.pancholi@promactinfo.com>
2016-07-06 17:16:34 +0530

    no msg

---
+++ mvc/Controllers/DemoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+       public ActionResult FillCustomer()
+        {
+            return View("DemoView");
+        }
+
+
+        public ActionResult DisplayCustomer()
+        {
+            DemoModel dm = new DemoModel();
+            dm.customer_id =Convert.ToInt16( Request.Form["cid"]);
+              dm.customer_name = Request.Form["cn"];
+             dm.customer_amount =Convert.ToDouble( Request.Form["camt"]);
+            
+            return View(dm);
+        }
+    }
+}
---
+++ mvc/Models/DemoModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace mvc.Models
+{
+
+   
+    public class DemoModel
+    {
+       
+        public int customer_id
+        {
+            get; set;
+        }
+
+        public String customer_name
+        {
+            get; set;
+        }
+
+
+        public Double customer_amount
+        {
+            get; set;
+        }
+
+
+    }
+}
